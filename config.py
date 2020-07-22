6f20b936b0f0e5b70d85a7e98c4612fd1bead119
Mingzhe Lin <mingzhaya@gmail.com>
2020-07-22 18:58:40 +0800

    User model for db

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+from pathlib import Path
+basedir = Path(__file__).parent.absolute()
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'rsrs_treasure_map'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                            'sqlite:///' + str(basedir / 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
