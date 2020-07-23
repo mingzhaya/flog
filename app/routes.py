8e651191f0f7a9dbf937d5da9838b425446ef693
Mingzhe Lin <mingzhaya@gmail.com>
2020-07-23 13:37:01 +0800

    Requiring login

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
 from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Mingles'}
     posts = [
@@ -28,7 +29,10 @@ def login():
             flash('Password does not exist')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/logout')
