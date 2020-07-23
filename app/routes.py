fe8ed75540455dd042870396b751dd2b9eb209f0
Mingzhe Lin <mingzhaya@gmail.com>
2020-07-23 15:55:11 +0800

    Register new user

--- app/routes.py
+++ app/routes.py
@@ -45,6 +45,12 @@ def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
-
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You have registered as {}.'.format(form.username.data))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
