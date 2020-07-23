24aee9f9676a8c4a6b9fd6202381ff44983193f7
Mingzhe Lin <mingzhaya@gmail.com>
2020-07-23 23:22:57 +0800

    Gather follower posts

--- app/models.py
+++ app/models.py
@@ -31,6 +31,23 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed_post = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own_post = Post.query.filter_by(user_id=self.id)
+        return followed_post.union(own_post).order_by(Post.timestamp.desc())
 
 @login.user_loader
 def load_user(id):
