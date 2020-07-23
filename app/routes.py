8d42f24e6064e2ef1bec31354b5c8f03f55cd8f6
Mingzhe Lin <mingzhaya@gmail.com>
2020-07-23 16:00:56 +0800

    Add profile page

--- app/routes.py
+++ app/routes.py
@@ -54,3 +54,13 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Post 1?'},
+        {'author': user, 'body': 'Post 222'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
