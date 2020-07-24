79b751029d3a1682b8793d1070a56b820bf6c9ee
Mingzhe Lin <mingzhaya@gmail.com>
2020-07-24 19:45:23 +0800

    Support pages

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                             'sqlite:///' + str(basedir / 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    POSTS_PER_PAGE = 5
