02884928cc99031da8675a5c5dd54358d173175b
Mingzhe Lin <mingzhaya@gmail.com>
2020-07-24 17:06:59 +0800

    Add exploration page

--- app/routes.py
+++ app/routes.py
@@ -20,6 +20,12 @@ def index():
     posts = current_user.followed_posts().all()
     return render_template('index.html', title='Home', form=form, posts=posts)
 
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Exploration', posts=posts)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
